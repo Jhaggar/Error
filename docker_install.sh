c066fc23f770661aa4f833be0afb8320609939e9
Ritesh Jhaggar <riteshjhaggar1988@gmail.com>
2021-03-03 17:42:12 +0530

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker version
+
+sudo docker run hello-world
